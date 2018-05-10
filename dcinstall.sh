66184bb652709270ed87c27c9537f13a28c0d976
mmliar <mmliar@users.noreply.github.com>
2018-05-10 10:22:35 -0700

    Update to datacube installation script
    Now the script supports remote database server

--- dcinstall.sh
+++ dcinstall.sh
@@ -2,7 +2,7 @@
 
 ENV="production"
 VERBOSE=false
-OUTPUT="datacube_install.log"
+OUTPUT="$HOME/datacube_install.log"
 CONDAQUIET=""
 
 echo "Datacube Installation Log" > ~/datacube_install.log
@@ -37,6 +37,68 @@ done
 echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 echo "@@      Welcome to Datacube      @@"
 echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo
+echo "Before installing datacube, you must install database server first."
+while true
+do
+echo -n "--> Have you installed database server? (y)es/(n)o "
+read ANSWER
+case $ANSWER in
+    no|No|NO|n|N)
+    echo "Please install database server first!"
+    echo "You may use dbinstall.sh to install database system."
+    exit 0
+    ;;
+    yes|Yes|YES|y|Y)
+    echo "Please provide your database server information to proceed"
+    break
+    ;;
+    *)
+    echo "Invalid answer."
+    ;;
+esac
+done
+
+while true
+do
+echo "Please enter the IP address of database server"
+echo -n "--> Enter database address: "
+read DBADD
+if [[ $DBADD != "" ]]
+then
+    break
+fi
+echo "Cannot accept empty database address!"
+done
+
+while true
+do
+echo "Please enter the username for database"
+echo -n "--> Enter new username: "
+read USERNAME
+if [[ $USERNAME != "" ]]
+then
+    break
+fi
+echo "Cannot accept empty username!"
+done
+
+while true
+do
+echo "Please enter the password for database"
+echo -n "--> Enter password: "
+read -s PASSWORD
+if [[ $PASSWORD != "" ]]
+then
+    break
+fi
+echo "Cannot accept empty password!"
+done
+
+echo 
+echo "Thank you! Now setting up datacube, this may take some time..."
+echo
+
 echo @ Requesting super user permission
 sudo echo
 
@@ -44,6 +106,7 @@ sudo echo
 cd
 
 echo Updating local system...
+sudo yum install -y deltarpm >> $OUTPUT
 sudo yum update -y >> $OUTPUT
 
 # Add IUS (Inline With Upstream) 3rd party repository for Python 3.6
@@ -61,7 +124,7 @@ sudo pip3.6 install pycosat pyyaml requests >> $OUTPUT
 
 # Conda used for package, dependency and environment management for any language, cross-platform
 echo Installing datacube environment. This may take some time...
-curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh >> $OUTPUT
+curl -s -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh >> $OUTPUT
 sh Miniconda3-latest-Linux-x86_64.sh -b >> $OUTPUT
 echo export PATH=\"$PWD/miniconda3/bin:\$PATH\" >> ~/.bashrc
 source ~/.bashrc
@@ -73,11 +136,23 @@ conda create --name cubeenv python=3.6 datacube -y $CONDAQUIET >> $OUTPUT
 # Scipi is a Python-based ecosystem of open-source software for mathematics, science, and engineering
 # Jupyter Notebook is an open-source web application that allows you to create and share documents
 # that contain live code, equations, visualizations and narrative text
-echo Installing development packages. This may take some time...
+echo "Installing development packages. This may take some time..."
 if [[ $ENV = 'development' ]]
 then
     conda install jupyter matplotlib scipy -y $CONDAQUIET >> $OUTPUT
 fi
 
+echo "Generating configuration File..."
+DATACUBECONFIGFILE="$HOME/.datacube.conf"
+echo "[datacube]" >> $DATACUBECONFIGFILE
+echo "db_database: datacube" >> $DATACUBECONFIGFILE
+echo "db_hostname: $DBADD" >> $DATACUBECONFIGFILE
+echo "db_username: $USERNAME" >> $DATACUBECONFIGFILE
+echo "db_password: $PASSWORD" >> $DATACUBECONFIGFILE
+
 echo
-echo Datacube has been installed. Log file has been saved to $PWD/datacube_install.log
+echo "Datacube has been installed."
+if [[ $VERBOSE = false ]]
+then
+    echo "Log file of this installation has been saved to $OUTPUT."
+fi
