e76705f6b3c2379699f37e51dd34f1940f3686d5
Tyler Hall <tyler@ionise.org>
2018-06-10 21:13:36 -0700

    need to strip 0 and 32767 to see real minmax

--- tools/get_minmax.py
+++ tools/get_minmax.py
@@ -22,16 +22,19 @@ for x in bands:
 
 for file in os.listdir(directory):
 	if file.endswith(".nc"):
+		#print(os.path.join(directory + "/", file))
 		for band in bands:
 			ds = gdal.Open('netCDF:' + os.path.join(directory, file) + ':' + band)
 			data = ds.GetRasterBand(1).ReadAsArray(0, 0, ds.RasterXSize, ds.RasterYSize)
 			data_min = np.amin(data[data != no_data])
 			data_max = np.amax(data[data != no_data])
-			min_values[band] = min_values[band] if min_values[band] else data_min
-			max_values[band] = max_values[band] if max_values[band] else data_max
-
-			if data_min < min_values[band]: min_values[band] = data_min
-			if data_max > max_values[band]: max_values[band] = data_max
+			
+			if data_min != 0:
+				min_values[band] = min_values[band] if min_values[band] else data_min
+				if data_min < min_values[band]: min_values[band] = data_min
+			if data_max != 32767:
+				max_values[band] = max_values[band] if max_values[band] else data_max
+				if data_max > max_values[band]: max_values[band] = data_max
 
 for band in bands:
 	print('{}: min = {}, max = {}'.format(band, min_values[band], max_values[band]))
